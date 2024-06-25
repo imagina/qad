49a1c77df87174cf196d222ec92d21d16a024791
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2024-06-25 17:40:54 -0500

    + add translations

--- _i18n/en-us/index.js
+++ _i18n/en-us/index.js
@@ -1,7 +1,82 @@
-import layout from 'modules/qad/_i18n/en-us/layout'
-import sidebar from 'modules/qad/_i18n/en-us/sidebar'
-
 export default {
-  layout,
-  sidebar
+  "iad": {
+    "cms": {
+      "sidebar": {
+        "adminGroup": "Ads",
+        "adminAds": "Ads List",
+        "adminAdsCreate": "Create Ad",
+        "adminAdsUpdate": "Update Ad",
+        "adminCategories": "Ads categories",
+        "adminUps": "Ads Ups",
+        "adminAdUps": "Ups Log",
+        "panelAds": "My Ads",
+        "checked": "Checked"
+      },
+      "create": {
+        "category": "Create category",
+        "service": "Create service",
+        "ad": "Create ad",
+        "up": "New Up"
+      },
+      "edit": {
+        "category": "Edit category",
+        "service": "Edit service",
+        "ad": "Edit ad",
+        "up": "Update Up"
+      },
+      "message": {
+        "automaticUploads": "¡Automatic Uploads!",
+        "chooseDefaultPrice": "You can select the price you want to appear by default in your ad."
+      },
+      "form": {
+        "user": "User",
+        "ad": "Ad",
+        "age": "Age",
+        "address": "Address",
+        "phone": "Phone",
+        "name": "Name",
+        "note": "Note",
+        "customerFeedback": "Customer Feedback",
+        "independentEscort": "independent Escort",
+        "services": "Services",
+        "tags": "Tags",
+        "status": "Status",
+        "photosAndVideo": "Photos and Videos",
+        "whereLocation": "Your Location",
+        "aboutAd": "About Your AD",
+        "newAd": "New Ad!",
+        "howContact": "How can contact you?",
+        "availableSchedule": "Available time",
+        "rates": "Rates",
+        "daysLimit": "Limit Of Days",
+        "upsDaily": "Ups Daily",
+        "adUps": "How pay your AD?",
+        "daysCounter": "Days Counter",
+        "upsCounter": "Upd Counter",
+        "fromDate": "From Date",
+        "toDate": "To Date",
+        "fromHour": "From Hour",
+        "toHour": "To Hour",
+        "nextUpAt": "Next Up At",
+        "rangeMinutes": "Go Up Every (Minutes)",
+        "featuredRate": "Featured Rate"
+      },
+      "options": {
+        "pending": "Pending",
+        "approved": "Approved",
+        "denied": "Denied"
+      },
+      "adCheck": {
+        "checked": "Congratulations! Your ad is verified",
+        "noRequested": "Your ad is not verified yet, you must create a request for it to be reviewed",
+        "requested": "Just a little more, we are verifying your ad",
+        "rejected": "Your ad could not be verified, check your information and recreate the request for it to be reviewed"
+      },
+      "documentation": {
+        "ads": "Ads are published on your WEB site. They are used to promote or boost some external product to the application",
+        "categories": "It is possible to create categories, with a name, description and organize them in tree mode (parent categories and child categories) in order to categorize all your plans.",
+        "ups": "When an announcement is published, it is put on a list that constantly grows with each new ad, with this functionality of Uploads it is possible to tell your application to improve the position of each ad with some definite rules,"
+      }
+    }
+  }
 }
