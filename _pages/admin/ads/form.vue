efdf2e6906fab81c979ef2d3af5312f4839b778e
msolanogithub <msolano2505@gmail.com>
2025-08-16 12:40:48 -0500

    replaced core setting name by isite

--- _pages/admin/ads/form.vue
+++ _pages/admin/ads/form.vue
@@ -677,7 +677,7 @@ export default {
           rules: [val => !!val || this.$tr('isite.cms.message.fieldRequired')],
           label: this.$tr('iprofile.cms.message.privacyData', {
             concatData: concatData,
-            siteName: this.$getSetting('core::site-name')
+            siteName: this.$getSetting('isite::site-name')
           })
         }
       };
